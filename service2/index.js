ed9dfe5d5a701e04d1c8225201ed7609b3a5cc9c
Landon Babay <landon.babay@outlook.com>
2023-06-14 16:22:58 -0400

    adding the three demo apps

---
+++ service2/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.get("/api/data", (req, res) => { 
+    res.status(200).json({msg: 'hello from service2'})
+})
+
+app.listen(3080, ()=> { 
+    console.log('listening on 3080')
+})
