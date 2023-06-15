dd6be69ec3270be8312763b96bd9e3d7538b4bc3
Landon Babay <landon.babay@outlook.com>
2023-06-15 14:37:01 -0400

    fixing some issues with formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react'
 import styled from 'styled-components'
+import {AiFillCloseCircle} from 'react-icons/ai'
+import {FaRegWindowClose} from 'react-icons/fa'
+
 
 function App() {
-  const [service1, setService1] = useState()
-  const [service2, setService2] = useState()
+  const [service1, setService1] = useState('')
+  const [service2, setService2] = useState('')
 
   const Wrapper = styled.div`
     color: #A0A0A0;
@@ -14,22 +17,40 @@ function App() {
       padding-bottom: 25px;
       border-bottom: 2px solid #FA8925;
     }
-    button { 
+    .trigger { 
       margin: 20px;
       width: 150px;
       border-radius: 15px;
       box-shadow: 5px 5px 5px;
     }
-    button:hover { 
+    .trigger:hover { 
       background-color: #FA8925;
     }
     h2{ 
       color: #FA8925;
     }
+    a { 
+      text-decoration: none;
+      color: #FA8925;
+      padding-left: 25px;
+    }
+    .button-container { 
+      display: flex;
+      align-items: center;
+      justify-content: center;
+     
+    }
+    .close { 
+      color: #FA8925;
+      text-decoration: none;
+      background-color: #36454F;
+      border: none;
+      padding-left: 35px;
+    }
   `
 
   const service1fetch = async () => { 
-      const data = await fetch("service1/api/data")
+      const data = await fetch("/service1/api/data")
       console.log(data)
       const response = await data.json();
       console.log(response.msg)
@@ -37,10 +58,18 @@ function App() {
   }
 
   const service2fetch = async () => { 
-    const data = await fetch("service2/api/data")
+    const data = await fetch("/service2/api/data")
     console.log(data)
     const response = await data.json()
-    setService2(response)
+    setService2(response.msg)
+  }
+
+  const closeService1 = async () => { 
+    setService1('')
+  }
+
+  const closeService2 = async () => { 
+    setService2('')
   }
 
   return (
@@ -50,12 +79,18 @@ function App() {
           <h1>Sample App</h1>
           <h3>react / node apps running in AKS</h3>
         </div>
-        <button onClick={()=> service1fetch()}>Service 1</button>
-        <button onClick={()=> service2fetch()}>Service 2</button>
+        <button className='trigger' onClick={()=> service1fetch()}>Service 1</button>
+        <button className='trigger' onClick={()=> service2fetch()}>Service 2</button>
       </div>
-      <div className='flex-item'> 
-          <h2>{service1}</h2>
-          <h2>{service2}</h2>
+      <div className='show-service1'>
+          {
+            service1 ? <div className='button-container'><h2>{service1}</h2><button className='close' onClick={()=>closeService1()}><AiFillCloseCircle /></button></div> : null  
+          }
+      </div>
+      <div className='show-service2'>
+        {
+          service2 ? <div className='button-container'><h2>{service2}</h2><button className='close' onClick={()=>closeService2()}><AiFillCloseCircle /></button></div> : null  
+        }
       </div>
     </Wrapper>
   )
