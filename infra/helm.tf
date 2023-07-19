ca6e65ee63e5965e789d30290c3a6feef3fd41c8
Landon Babay <landon.babay@outlook.com>
2023-07-19 12:46:13 -0400

    working on CICD pipelines

---
+++ infra/helm.tf
@@ -0,0 +1,21 @@
+locals { 
+    argo = "argocd"
+    argo_repo = "https://argoproj.github.io/argo-helm"
+    argo_chart = "argo-cd"
+    argo_namespace = "argocd"
+}
+
+resource "helm_release" "argocd" { 
+    name = local.argo 
+    repository = local.argo_repo 
+    chart = local.argo_chart 
+    namespace = local.argo_namespace 
+    create_namespace = true 
+
+    values = [ 
+        file("manifests/argo-app.yml")
+    ]
+    depends_on = [ 
+        module.eks-cluster
+    ]
+}
