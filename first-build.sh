e05c86b91fe4e00d78fa1244bf5032b84b80a0ed
Landon Babay <landon.babay@outlook.com>
2023-07-20 15:37:17 -0400

    adding the 1.0 image

--- first-build.sh
+++ first-build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash 
-REPOSITORIES="frontend service1 service2"
+for repo in frontend service1 service2 
+do 
+    aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 500731508494.dkr.ecr.us-east-2.amazonaws.com
+    cd $repo 
+    docker build -t 500731508494.dkr.ecr.us-east-2.amazonaws.com/$repo:1.0 .
+    docker push 500731508494.dkr.ecr.us-east-2.amazonaws.com/$repo:1.0
+    cd .. 
+done
